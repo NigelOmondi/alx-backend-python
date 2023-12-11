9523e03c75e544f2b9d6c92fd65cb8e21849c6e0
NigelOmondi <nigelomondi62@gmail.com>
2023-12-11 18:01:33 +0300

    Async tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -22,4 +22,4 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
         List[float]: The list of delays in ascending order.
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*tasks)
+    return [await task for task in asyncio.as_completed(tasks)]
